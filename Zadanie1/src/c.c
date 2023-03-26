da2519d8d59893f7b1e958646b05ef2d80e680c2
Patryk Lach <patryk.lach@mp.pl>
2023-03-26 13:56:38 +0200

    fix(c): sleep longer

--- Zadanie1/src/c.c
+++ Zadanie1/src/c.c
@@ -14,9 +14,10 @@ int main() {
 
         if (pid == -1) {
             perror("fork error");
+            exit(-1);
         } else if (pid == 0) {
             printf("Proces potomny: PID=%d PPID=%d UID=%d GID=%d PGID=%d\n", getpid(), getppid(), getuid(), getgid(), getpgid(0));
-            sleep(10);
+            sleep(60);
         }
     }
 
