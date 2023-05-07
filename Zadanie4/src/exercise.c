8d099edc5e0ce4db2abec4403d239fb718a22cb3
Patryk Lach <patryk.lach@mp.pl>
2023-05-07 12:40:24 +0200

    refactor(zadanie4): comments

--- Zadanie4/src/exercise.c
+++ Zadanie4/src/exercise.c
@@ -18,39 +18,30 @@ int main(int argc, char **argv)
     char consumer_buffer[BUFFER_SIZE];
     ssize_t bytes_read, bytes_written;
     pid_t pid;
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
 
-    // otwórz plik wejściowy
     int input_fd = open(argv[1], O_RDONLY);
     if (input_fd == -1)
     {
-        perror("open");
+        perror("błąd przy otwieraniu!");
         exit(EXIT_FAILURE);
     }
 
-    // utwórz potok
     if (pipe(pipefd) == -1)
     {
-        perror("pipe");
+        perror("błąd w pipe!");
         exit(EXIT_FAILURE);
     }
 
-    // rozdziel proces
     pid = fork();
     if (pid == -1)
     {
-        perror("fork");
+        perror("błąd w fork!");
         exit(EXIT_FAILURE);
     }
 
-    // Producent
     if (pid == 0)
     {
-        close(pipefd[0]); // zamknij nieużywany koniec potoku
+        close(pipefd[0]);
 
         // czytaj dane z pliku i zapisuj do potoku
         while ((bytes_read = read(input_fd, producer_buffer, BUFFER_SIZE)) > 0)
@@ -61,19 +52,19 @@ int main(int argc, char **argv)
                 exit(EXIT_FAILURE);
             }
             printf("Producent wysłał: %s\n", producer_buffer);
-            // losowe opóźnienie
+            
             sleep(rand() % (3 - 0 + 1) + 0); // losowo od 0 do 3 sec
         }
 
-        close(input_fd);  // zamknij plik wejściowy
-        close(pipefd[1]); // zamknij koniec potoku
+        close(input_fd);
+        close(pipefd[1]);
         exit(EXIT_SUCCESS);
     }
 
     // Konsument
     else
     {
-        close(pipefd[1]); // zamknij nieużywany koniec potoku
+        close(pipefd[1]);
 
         // otwórz plik wyjściowy
         int output_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
@@ -92,12 +83,12 @@ int main(int argc, char **argv)
                 exit(EXIT_FAILURE);
             }
             printf("Konsument otrzymał: %s\n", consumer_buffer);
-            // losowe opóźnienie
+            
             sleep(rand() % (3 - 0 + 1) + 0); // losowo od 0 do 3 sec
         }
 
-        close(output_fd); // zamknij plik wyjściowy
-        close(pipefd[0]); // zamknij koniec potoku
+        close(output_fd);
+        close(pipefd[0]);
         printf("Konsument zakończył pracę.\n");
         exit(EXIT_SUCCESS);
     }
